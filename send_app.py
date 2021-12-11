c4c21d17c6e8007d083de43c22ae3f20aa31f7f7
Luigi Brancati <luigi.brancati93@gmail.com>
2021-12-11 23:11:10 +0100

    selenium

--- send_app.py
+++ send_app.py
@@ -1,6 +1,10 @@
+from selenium import webdriver
 from resume import ResumeGenerator
 from fake_contacts import FakeIdentity
 
-fake_id = FakeIdentity()
-resume = ResumeGenerator(fake_id.curriculum(), fake_id.contacts())
-resume.generate()
+# fake_id = FakeIdentity()
+# resume = ResumeGenerator(fake_id.curriculum(), fake_id.contacts())
+# resume.generate()
+
+driver = webdriver.Firefox()
+driver.get("www.python.org")
